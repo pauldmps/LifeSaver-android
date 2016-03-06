5b09a2bcff641dba40e7c0b9bf290890e8f38f7d
Shantanu Paul <shantanupaul@live.com>
2016-03-06 19:13:00 +0530

    Revert "Demo"
    This reverts commit 599c2522c021f83d0cebc5e97f5af5388093a2d6.

--- app/src/main/java/com/paulshantanu/lifesaver/MapsActivity.java
+++ app/src/main/java/com/paulshantanu/lifesaver/MapsActivity.java
@@ -88,8 +88,6 @@ public class MapsActivity extends Fragment implements OnMapReadyCallback {
                     }
                     catch (Exception ex){
                         ex.printStackTrace();}
-
-
                 }
             }
         }).execute();
